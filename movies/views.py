f65b647f8adfebd1e95aaef4eb88659e25da9eb1
dima <dimdirim95@gmail.com>
2023-06-24 22:38:01 +0200

    add template and static

--- movies/views.py
+++ movies/views.py
@@ -8,6 +8,6 @@ class MoviesView(View):
     """Movies list"""
     def get(self, request):
         movies = Movie.objects.all()
-        return render(request, 'movies/movie_list.html', {'movies_list': movies})
+        return render(request, 'movies/movies.html', {'movies_list': movies})
     
 
